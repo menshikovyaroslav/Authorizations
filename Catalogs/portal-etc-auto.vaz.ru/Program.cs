42e67f5b7f31dff60ce05c13ddfbf42c85fe97d1
menshikovyaroslav <menshikov.yaroslav@gmail.com>
2020-09-05 14:04:26 +0700

    Added portal-etc-auto.vaz.ru

---
+++ Catalogs/portal-etc-auto.vaz.ru/Program.cs
@@ -0,0 +1,62 @@
+﻿using CatalogSupportLibrary;
+using CatalogSupportLibrary.Requests;
+using Dom.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace portal_etc_auto.vaz.ru
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Each vaz account has two parameters : User, Password
+            var user = "user";              // !!! rewrite value from your account !!!
+            var password = "password";      // !!! rewrite value from your account !!!
+
+            // We keep cookies here
+            var cookies = new CookieContainer();
+
+            // Any proxy, for example Fiddler
+            var proxy = new WebProxy("127.0.0.1:8888");
+
+            // auth request
+            var data = $"id2_hf_0=&username={user}&password={password}&search=%D0%92%D0%BE%D0%B9%D1%82%D0%B8";
+            var postRequest = new PostRequest()
+            {
+                Data = data,
+                Address = $"http://portal-etc-auto.vaz.ru:7003/login?0-1.IFormSubmitListener-signIn-signInForm",
+                Accept = "text/html, application/xhtml+xml, */*",
+                Host = "portal-etc-auto.vaz.ru:7003",
+                ContentType = "application/x-www-form-urlencoded",
+                Referer = "http://portal-etc-auto.vaz.ru:7003/login",
+                KeepAlive = true,
+                Proxy = proxy
+            };
+            postRequest.Run(ref cookies);
+
+            // writing auth response in log
+            Config.Instance.AddLogInfo($"vaz auth answer: {postRequest.Response}");
+
+            // comfortable cookies presentation
+            var catalogCookies = cookies.GetCookieCollection();
+
+            // writing cookies in log
+            foreach (var cookie in catalogCookies)
+            {
+                Config.Instance.AddLogInfo($"cookie: {cookie}");
+            }
+
+            var result = "unknown";
+            if (postRequest.Response.Contains("news")) result = "Authentication successful";
+            else result = "Authentication failed";
+
+            // writing Authentication result status in log
+            Config.Instance.AddLogInfo($"Auth result: {result}");
+        }
+    }
+}
---
+++ Catalogs/portal-etc-auto.vaz.ru/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Управление общими сведениями о сборке осуществляется с помощью 
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("portal-etc-auto.vaz.ru")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("portal-etc-auto.vaz.ru")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Параметр ComVisible со значением FALSE делает типы в сборке невидимыми 
+// для COM-компонентов.  Если требуется обратиться к типу в этой сборке через 
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("38a356ac-beb4-4984-b52a-eefb305f5262")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии 
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
