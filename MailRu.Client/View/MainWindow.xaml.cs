7c146469e4b42fe9e896dfacefc85658a6421994
menshikovyaroslav <menshikov.yaroslav@gmail.com>
2020-12-02 23:47:58 +0700

    MainWindow removed into View

--- MailRu.Client/View/MainWindow.xaml.cs
+++ MailRu.Client/View/MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace MailRu.Client
+namespace MailRu.Client.View
 {
     /// <summary>
     /// Логика взаимодействия для MainWindow.xaml
